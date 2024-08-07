ccb590c529db5fd1a63732a23f31602c4d5b0e9d
Hyeok-gu <thanatos6737@naver.com>
2024-08-07 14:51:02 +0900

    환경변수 설정

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoB7jPca5UoD--vYdTE43HDZgqrwmDJBI",
-  authDomain: "icecreamfactory-fbe1b.firebaseapp.com",
-  projectId: "icecreamfactory-fbe1b",
-  storageBucket: "icecreamfactory-fbe1b.appspot.com",
-  messagingSenderId: "33392143586",
-  appId: "1:33392143586:web:d8d0bec618cd4c377602bb",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
